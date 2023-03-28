d8bcc1e5e6af0185b170fdf85fc22b3fe91d642e
Awary <Nolofinve1991@gmail.com>
2023-03-28 15:46:10 +0500

    Нарешал задач

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿/*Console.WriteLine("Введите число");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Вы ввели число ");
+
+Console.Write(a);
+
+Console.WriteLine("Введите число");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.Write(a);
+
+Console.Write(" - вот какое число Вы ввели");*/
+
+/*Console.WriteLine("Введите расстояние в см");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = a / 100;
+
+Console.Write("Число полных метров ");
+
+Console.Write(b);*/
+
+/*Console.WriteLine("Введите количество прошедших дней");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = a / 7;
+
+Console.Write("Число полных недель прошло ");
+
+Console.Write(b);*/
+
+/*Console.WriteLine("Сколько секунд с начала дня прошло?");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = a / 3600;
+
+Console.Write("Число полных часов прошло ");
+
+Console.WriteLine(b);
+
+int c = a / 60;
+
+Console.Write("Число полных минут прошло ");
+
+Console.WriteLine(c);*/
+
+/*Console.WriteLine("Введите первое число");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int b = int.Parse(Console.ReadLine());
+
+int c = a / b;
+
+Console.WriteLine(a);
+
+Console.Write("/");
+
+Console.Write(b);
+
+Console.Write("=");
+
+Console.WriteLine(c);
+
+int d = a % b;
+
+Console.Write(a);
+
+Console.Write("%");
+
+Console.Write(b);
+
+Console.Write("=");
+
+Console.Write(d);*/
+
+/*Console.WriteLine("Введите первое число");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int b = int.Parse(Console.ReadLine());
+
+int c = (a % b + b);
+
+Console.WriteLine(c);*/
+
+
+
