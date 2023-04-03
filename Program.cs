92a4b600f3796fed16311f552566ee9893f1db6d
Awary <Nolofinve1991@gmail.com>
2023-04-03 11:55:58 +0500

    Домашняя работа 4 семинара

--- Program.cs
+++ Program.cs
@@ -121,3 +121,95 @@ b = (a / b + b);
 int c = (a % b + a);
 
 Console.WriteLine(c);*/
+
+/*int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 >result) result = arg2;
+    if(arg3 >result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+
+int b1 = 21;
+
+int c1 = 39;
+
+int a2 = 12;
+
+int b2 = 23;
+
+int c2 = 33;
+
+int a3 = 13;
+
+int b3 = 23;
+
+int c3 = 33;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+Console.WriteLine(max);*/
+
+
+
+/*Console.WriteLine("Введите пятизначное число");
+
+string num = Console.ReadLine();
+
+if (num[0] == num[4] & num[1] == num[3])
+    {
+        Console.WriteLine("Это полиндром");
+    }
+else 
+    Console.WriteLine("не полиндром");*/
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//Console.WriteLine("Введите число");
+
+//int a = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <= a; i++)
+//{
+//    Console.WriteLine(i * i * i);
+//}
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*Console.WriteLine("Введите x1");
+
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y1");
+
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z1");
+
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите x2");
+
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y2");
+
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z2");
+
+int z2 = int.Parse(Console.ReadLine());
+
+int a = (x2 - x1);
+
+int b = (y2 - y1);
+
+int c = (z2 - z1);
+
+double range = Math.Sqrt((a * a) + (b * b) + (c * c));
+
+Console.WriteLine(range);*/
